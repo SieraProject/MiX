43274fba7695d25a1d18fb80eb41c3e565f23b19
Siera <padangbaysa@gmail.com>
2023-04-09 03:57:53 +0700

    Ex vending

--- sql-files/extended_vending.sql
+++ sql-files/extended_vending.sql
@@ -4,3 +4,4 @@ ALTER TABLE `vendings` ADD COLUMN `extended_vending_item` int(10) unsigned NOT N
 -- REPLACE INTO `item_db2_re` (`id`,`name_aegis`,`name_english`,`type`,`price_sell`,`weight`) VALUES (30001,'Cash','Cash','Etc',10,10);
 -- REPLACE INTO `item_db2` (`id`,`name_aegis`,`name_english`,`type`,`price_sell`,`weight`) VALUES (30000,'Zeny','Zeny','Etc',10,10);
 -- REPLACE INTO `item_db2` (`id`,`name_aegis`,`name_english`,`type`,`price_sell`,`weight`) VALUES (30001,'Cash','Cash','Etc',10,10);
+-- REPLACE INTO `item_db2` (`id`,`name_aegis`,`name_english`,`type`,`price_sell`,`weight`) VALUES (30001,'Cash','Cash','Etc',10,10);
